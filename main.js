02255d23c2cd7d5a5e16a5462d6692f3f00b8afc
vai na web <vainawebcpx@gmail.com>
2019-10-23 17:23:27 -0300

    HBG

---
+++ main.js
@@ -0,0 +1,9 @@
+let lista = document.querySelector('#nav')
+
+function abrirMENU(){
+	lista.style.display = 'block';
+	open.style.display = 'flex';
+	close.style.display = 'none'
+	console.log('none'nav)
+
+}
